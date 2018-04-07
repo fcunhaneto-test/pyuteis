f050c21df82f1df47537901000ea562e6ef19518
Francisco Cunha Neto <fcunhaneto@gmail.com>
2018-04-07 13:56:01 -0300

    upgrade clear()

--- uteis.py
+++ uteis.py
@@ -1,4 +1,8 @@
-from os import system
+import os
 
 def clear():
-    system('cls||clear')
+    name = os.name
+    if name == 'posix':
+        os.system('clear')
+    elif name == 'nt' or name == 'dos':
+        os.system('cls')
